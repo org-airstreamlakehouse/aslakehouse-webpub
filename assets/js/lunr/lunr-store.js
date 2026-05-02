5f184e1ca4b0baa1c5819d83bba203e6c52f3d3d
daidevsb <daidevsb@users.noreply.github.com>
2026-05-02 23:29:26 +0000

    Deploy from aslakehouse-src@ce80c74ba8d3ccbb48ad5d68f9c2313a895b35e3 org-airstreamlakehouse/aslakehouse-src@ce80c74ba8d3ccbb48ad5d68f9c2313a895b35e3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Welcome to Airstream Lakehouse!",
+        "excerpt":"   Welcome to our Airstream Lakehouse site which is a work in progress      This is not our Airstream.  We have more solar than this one.  You can find this picture here https://www.airstream.com/blog/power-101-how-solar-power-works-in-an-airstream/  ","categories": ["blog"],
+        "tags": ["chat"],
+        "url": "/blog/welcome-to-aslh/",
+        "teaser": null
+      },{
+        "title": "Picking up our Airstream!",
+        "excerpt":"   Orientation and picking up our Airstream at Airstream Adventures NW      Hooking up our Airstream for the first time     ","categories": ["blog"],
+        "tags": ["pics"],
+        "url": "/blog/picking-it-up/",
+        "teaser": null
+      },{
+        "title": "Video of our Airstream!",
+        "excerpt":"   Couple videos of our Airstream   Orientation and Walk Through            Rolling down the highway            ","categories": ["blog"],
+        "tags": ["video"],
+        "url": "/blog/some-videos/",
+        "teaser": null
+      },{
+        "title": "Fixing the pairing on the Airstream backup camera",
+        "excerpt":"Pairing Voyager WiSight 2.0 Backup Camera Pair, Plug-in, Depart This video was really helpful to me tonight to pair the Voyager WiSight 2.0 Backup Camera on our Airstream. It is focused on the camera being used on trucks but the advice is the same for the Airstream. What is important...","categories": ["blog"],
+        "tags": ["chat"],
+        "url": "/blog/fixing-the-pairing/",
+        "teaser": null
+      },{
+        "title": "Camping at Kayak Point Regional County Park",
+        "excerpt":"Leaving our Campsite at Kayak Point Regional County Park We had a great weekend camping over the Presidents Day weekend. Saturday was dry, but it rained pretty steady Sunday. Kayak Point Regional County Park is open year-round and offers ten yurts and 30 partial-utility (water/power) in mostly large, wooded campsites...","categories": ["blog"],
+        "tags": ["video"],
+        "url": "/blog/kayak-point/",
+        "teaser": null
+      },{
+        "title": "New Logo for Airstream Lakehouse created on Microsoft Bing Image Creator powered by DALL-E 3",
+        "excerpt":"New Logo for Airstream Lakehouse created on Microsoft Bing Image Creator powered by DALL-E 3 Decided to use DALL-E 3 to create a new logo for the Airstream Lakehouse blog. Just provided a prompt describing what I was looking for, and after editing the prompt, on my 3rd try I...","categories": ["blog"],
+        "tags": ["pics"],
+        "url": "/blog/dalle3-airstreamlakehouse/",
+        "teaser": null
+      }]
